36910409a53c5fee5ca25f043d7f81f87483e70a
Sean Antony <seanantonyjr11@gmail.com>
2024-08-03 14:52:52 +0530

    left panel update

--- india-post/src/App.js
+++ india-post/src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/leftpanel'
+import LeftPanel from './components/leftpanel';
 
 function App() {
   return (
     <div className="App">
       <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      Hello world!
+      </h1>
+      <div className='left'>
+        <LeftPanel/>
+      </div>
     </div>
   );
 }
--- india-post/src/components/leftpanel.js
+++ india-post/src/components/leftpanel.js
@@ -5,11 +5,13 @@ const LeftPanel = () => {
     return(
         <div className='left-panel'>
             <div className='menu'>
-
+                <h2>Menu</h2>
             </div>
             <div className='footer'>
-                
+                <h2>Footer</h2>
             </div>
         </div>
     )
 }
+
+export default LeftPanel
