5f73bef3bf7465e431f7ac2fba5d7e56d8b8697b
Sean Antony <seanantonyjr11@gmail.com>
2024-08-04 22:23:42 +0530

    login page 1 added

--- india-post/src/App.js
+++ india-post/src/App.js
@@ -1,34 +1,11 @@
-import './App.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import LoginPage from './components/pages/loginpage';
-import SignupPage from './components/pages/signuppage';
-import LoginLeftPanel from './components/LoginLeftPanel';
+import Login from './pages/login';
 
 function App() {
-  return (
-    <><LoginLeftPanel /><div className="App min-h-screen flex">
-      {/* Left Panel */}
-      <div className="w-1/3">
-
-      </div>
-
-      {/* Main Content */}
-      <div className="w-2/3 flex items-center justify-center">
-        <div className="max-w-md w-full space-y-8">
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<LoginPage />} />
-              <Route path="/signup" element={<SignupPage />} />
-            </Routes>
-          </BrowserRouter>
-        </div>
-      </div>
-    </div></>
-  );
+  return(
+  <div className='App'>
+    <Login />
+  </div>
+  )
 }
 
 export default App;
