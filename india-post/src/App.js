e29249f131c4e4ca52df1b80dd9f7b8555b3880e
FernandezFigo105 <110619874+FernandezFigo105@users.noreply.github.com>
2024-08-05 16:05:34 +0530

    Tailwind config and Home page changed

--- india-post/src/App.js
+++ india-post/src/App.js
@@ -7,27 +7,24 @@ import {
 import LoginPage from './components/pages/loginpage';
 import SignupPage from './components/pages/signuppage';
 import LoginLeftPanel from './components/LoginLeftPanel';
+import Home from './components/pages/home.js';
 
 function App() {
   return (
-    <><LoginLeftPanel /><div className="App min-h-screen flex">
-      {/* Left Panel */}
-      <div className="w-1/3">
-
-      </div>
+    <><Home/>
+   
 
       {/* Main Content */}
-      <div className="w-2/3 flex items-center justify-center">
+      {/* <div className="w-2/3 flex items-center justify-center">
         <div className="max-w-md w-full space-y-8">
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<LoginPage />} />
               <Route path="/signup" element={<SignupPage />} />
             </Routes>
-          </BrowserRouter>
-        </div>
-      </div>
-    </div></>
+          </BrowserRouter> */}
+       
+    </>
   );
 }
 
