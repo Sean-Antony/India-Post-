afe780b60a4d27722718d35b671873c59e3746cf
Sean Antony <104753783+Sean-Antony@users.noreply.github.com>
2024-09-09 21:09:27 +0530

    responsi

--- india-post/src/pages/features.js
+++ india-post/src/pages/features.js
@@ -22,99 +22,101 @@ const Features = () => {
   return (
     <div className='flex flex-col lg:flex-row min-h-screen bg-[#F7F7F9]'>
       <LeftPanel currentStep={currentStep}/>
-      <div className='flex-1 flex flex-col xl:mx-3'>
-        <header className="flex items-center justify-between p-4 lg:mx-16 xl:mr-[80px]">
+      <div className='flex-1 flex flex-col '>
+        <header className="flex items-center justify-between py-5 px-14">
           <img 
             src={logo}
             alt='India Post Logo' 
-            className='h-[100px] w-auto'
+            className='h-[80px] 2xl:h-[100px] w-auto'
           />
           <div className='flex space-x-4'>
-            <button type="button" className="border border-k1 bg-[#F7F7F9] text-black font-bold py-2 px-4 rounded-md flex items-center">
+            <button type="button" className="border border-k1 bg-[#F7F7F9] h-[40px] 2xl:h-[50px] text-black font-bold py-2 px-4 rounded-md flex items-center">
               <ErrorRoundedIcon className='mr-2 text-k1' />
                Quick Access
             </button>
-            <button type="button" className="border border-k1 bg-[#F7F7F9] text-black font-bold py-2 px-4 rounded-md flex items-center">
+            <button type="button" className="border border-k1 bg-[#F7F7F9] h-[40px] 2xl:h-[50px] text-black font-bold py-2 px-4 rounded-md flex items-center">
               <HelpRoundedIcon className='mr-2 text-k1' />
               Contact Help
             </button>
               <div className='flex justify-center'>
                 <div className='h-[50px] w-px bg-gray-300'></div>
               </div>
-              <button type="button" className='border border-gray-300 bg-white py-3 px-3 rounded-md flex items-center'>
-                <BiSolidMessageDots className='text-[25px] text-gray-400'/>
+              <button type="button" className='border border-gray-300 bg-white h-[40px] w-[40px] 2xl:h-[50px] 2xl:[50px] p-3 rounded-md flex items-center'>
+                <BiSolidMessageDots className='text-[25px] text-gray-400' />
               </button>
-              <button type="button" className='border border-gray-300 bg-white py-3 px-3 rounded-md flex items-center'>
+              <button type="button" className='border border-gray-300 bg-white h-[40px] w-[40px] 2xl:h-[50px] 2xl:[50px] p-3 rounded-md flex items-center'>
                 <FaBell className='text-[25px] text-gray-400'/>
               </button>
           </div>
         </header>
-        <div className='flex justify-center '>
-          <div className='h-px w-[1200px] bg-gray-300 mb-[30px]'></div>
+        <div className='flex justify-center px-14'>
+          <div className='h-px w-full bg-gray-300 mb-5 2xl:mb-[30px]'></div>
         </div>
-            <div className='flex flex-col justify-between px-[90px] space-y-4'>
+            <div className='flex flex-col justify-between  space-y-4 mx-14'>
               <h2 className='font-PB text-lg '>Welcome Back, Samvith</h2>
-              <div className='flex flex-row justify-between'>
-                <card className='flex h-[180px] w-[375px] justify-between items-center border border-gray-300 rounded-md bg-white'>
-                  <div className="flex items-center justify-center h-12 w-12 rounded-full overflow-hidden ml-[40px] mr-[-60px] mt-[-60px]">
+              <div className='grid grid-cols-10 gap-4 justify-between'>
+                <card className='flex flex-row col-span-3 h-30 2xl:h-48 p-4 justify-between items-center border border-gray-300 rounded-md bg-white'>
+                  <div className="flex items-center justify-center h-10 w-10 2xl:h-12 2xl:w-12 rounded-full overflow-hidden">
                   <img className="h-full w-full object-cover" src={logo} alt='Profile' />
                   </div> 
                   <div className='flex flex-col space-y-5'>
                     <div>
-                    <h2 className='text-lg font-PSB'>Samvith Raj</h2>
+                    <h2 className='text-md 2xl:text-lg font-PSB'>Samvith Raj</h2>
                     <h3 className='text-sm text-gray-400 font-PSB'>ID: 923045734</h3>
                     </div>
                     <div>
                     <h3 className='text-sm text-gray-400 font-PSB'>Office ID</h3>
-                    <h2 className='text-lg font-PSB'>SP2908512653</h2>
+                    <h2 className='text-md 2xl:text-lg font-PSB'>SP2908512653</h2>
                     </div>
                   </div>
-                  <button type="button" className='bg-white py-2 px-3 rounded-md flex items-center text-gray-500 mr-[20px] ml-[-60px] mt-[-80px]'>
-                  <MoreHorizRoundedIcon fontSize='large'/>
-                  </button>
+                  <div>
+                    <button type="button" className='bg-white py-2 px-3 rounded-md flex items-center text-gray-500 '>
+                    <MoreHorizRoundedIcon fontSize='large'/>
+                    </button>
+                  </div>
                 </card>
-                <card className='flex h-[180px] w-[800px] justify-between items-center border border-gray-300 rounded-md bg-white'>
+                <card className='flex col-span-7 justify-between items-center border border-gray-300 rounded-md bg-white'>
                   <div className='flex flex-row justify-between space-x-[80px] ml-[50px]'>
                     <div className='flex flex-col justify-between space-y-5'>
                       <div>
                         <h3 className='text-sm text-gray-400 font-PSB'>Postal Network</h3>
-                        <h2 className='text-lg font-PSB'>Ahmedabad NSH</h2>
+                        <h2 className='text-md 2xl:text-lg font-PSB'>Ahmedabad NSH</h2>
                       </div>
                       <div>
                         <h3 className='text-sm text-gray-400 font-PSB'>Counter No</h3>
-                        <h2 className='text-lg font-PSB'>1</h2>
+                        <h2 className='text-md 2xl:text-lg font-PSB'>1</h2>
                       </div>                     
                     </div>
                     <div className='flex flex-col justify-between space-y-5'>
                       <div>
                         <h3 className='text-sm text-gray-400 font-PSB'>Email ID</h3>
-                        <h2 className='text-lg font-PSB'>samvith12@gmail.com</h2>
+                        <h2 className='text-md 2xl:text-lg font-PSB'>samvith12@gmail.com</h2>
                       </div>
                       <div>
                         <h3 className='text-sm text-gray-400 font-PSB '>Phone</h3>
-                        <h2 className='text-lg font-PSB '>+91 9832472384</h2>
+                        <h2 className='text-md 2xl:text-lg font-PSB '>+91 9832472384</h2>
                       </div>                     
                     </div>
                     <div className='flex flex-col justify-between space-y-5'>
                     <div>
                         <h3 className='text-sm text-gray-400 font-PSB'>Bio</h3>
-                        <h2 className='text-lg font-PSB'>Counter Manager</h2>
+                        <h2 className='text-md 2xl:text-lg font-PSB'>Counter Manager</h2>
                       </div>
                       <div>
                         <h3 className='text-sm text-gray-400 font-PSB '>Location</h3>
-                        <h2 className='text-lg font-PSB '>Ahmedabad</h2>
+                        <h2 className='text-md 2xl:text-lg font-PSB '>Ahmedabad</h2>
                       </div>                     
                     </div>
                   </div>
                 </card>                
               </div>
             </div>
-            <div className='flex justify-center '>
-              <div className='h-px w-[1200px] bg-gray-300 mt-[30px] mb-[15px]'></div>
+            <div className='flex justify-center px-14'>
+              <div className='h-px w-full bg-gray-300 mt-[30px] mb-[15px]'></div>
             </div>
-            <div className='flex items-center justify-between mr-[90px] mb-[15px]'>
-            <div className='flex flex-col justify-between px-[90px]'>
-              <h2 className='font-PB text-lg '>Services</h2>
+            <div className='flex items-center justify-between px-14 mb-[15px]'>
+            <div className='flex flex-col justify-between '>
+              <h2 className='font-PB text-md 2xl:text-lg '>Services</h2>
               <div className='flex flex-row'>
               <h3 className='text-md text-gray-400 font-PSB '>Counter No:</h3>
               <h3 className='text-md font-PB ml-1'>1</h3>
@@ -127,56 +129,56 @@ const Features = () => {
               </button>
               </div>
             </div>
-            <div className='grid grid-cols-3 gap-4 ml-[90px] mr-[70px]'>
-          <div className='mb-2'>
-          <Card
-            icon={MailRoundedIcon}
-            bgColor="bg-[#B01E1D]"
-            title="Mail"
-            description="Effortlessly send and receive mail with seamless tracking and delivery."
-          />
-        </div>
-        <div className='mb-2'>
-          <Card
-            icon={LanguageIcon}
-            bgColor="bg-[#FCA61F]"
-            title="International Mail"
-            description="Expand your reach globally with secure international mail services."
-            />
-        </div>
-        <div className='mb-2'>
-          <Card
-            icon={ArticleIcon}
-            bgColor="bg-[#7288FF]"
-            title="Finance & Insurance"
-            description="Manage your finances and protect your shipment with ease."
-          />
-        </div>
-        <div className='mb-2'>
-          <Card
-            icon={CreditCardRoundedIcon}
-            bgColor="bg-[#bf6ef1]"
-            title="E-Payments"
-            description="Conveniently handle payments for postage and other services online."
-          />
-        </div>
-        <div className='mb-2'>
-          <Card
-            icon={TimelineRoundedIcon}
-            bgColor="bg-[#6cedc4]"
-            title="Product Sales"
-            description="Explore a wide range of postal products and services tailored to your needs."
-          />
-        </div>
-        <div className='mb-2'>
-          <Card
-            icon={FileCopyRoundedIcon}
-            bgColor="bg-[#8a76fc]"
-            title="Compare & Book"
-            description="Find the best postal services for your requirements and book with confidence."
-          />
-          </div>
-        </div>
+            <div className='grid grid-cols-3 gap-x-14 gap-y-4 px-14'>
+              <div className='mb-2'>
+              <Card
+                icon={MailRoundedIcon}
+                bgColor="bg-[#B01E1D]"
+                title="Mail"
+                description="Effortlessly send and receive mail with seamless tracking and delivery."
+              />
+            </div>
+            <div className='mb-2'>
+              <Card
+                icon={LanguageIcon}
+                bgColor="bg-[#FCA61F]"
+                title="International Mail"
+                description="Expand your reach globally with secure international mail services."
+                />
+            </div>
+            <div className='mb-2'>
+              <Card
+                icon={ArticleIcon}
+                bgColor="bg-[#7288FF]"
+                title="Finance & Insurance"
+                description="Manage your finances and protect your shipment with ease."
+              />
+            </div>
+            <div className='mb-2'>
+              <Card
+                icon={CreditCardRoundedIcon}
+                bgColor="bg-[#bf6ef1]"
+                title="E-Payments"
+                description="Conveniently handle payments for postage and other services online."
+              />
+            </div>
+            <div className='mb-2'>
+              <Card
+                icon={TimelineRoundedIcon}
+                bgColor="bg-[#6cedc4]"
+                title="Product Sales"
+                description="Explore a wide range of postal products and services tailored to your needs."
+              />
+            </div>
+            <div className='mb-2'>
+              <Card
+                icon={FileCopyRoundedIcon}
+                bgColor="bg-[#8a76fc]"
+                title="Compare & Book"
+                description="Find the best postal services for your requirements and book with confidence."
+              />
+              </div>
+            </div>
       </div>
     </div>
   )
--- india-post/src/pages/login.js
+++ india-post/src/pages/login.js
@@ -53,7 +53,7 @@ function Login() {
           <img
             src={logo}
             alt="India Post Logo"
-            className="h-[80px] sm:h-[100px] w-auto"
+            className="h-[80px] 2xl:h-[100px] "
           />
           <div className="flex space-x-4">
             <button
@@ -75,18 +75,18 @@ function Login() {
         <div className="flex justify-center">
           <div className="h-[2px] w-full max-w-screen-xl bg-gray-200"></div>
         </div>
-        <div className="flex items-center justify-center mt-8 lg:mt-20">
-          <div className="bg-white rounded-lg border p-6 sm:p-8 lg:p-10 w-full max-w-md sm:max-w-lg xl:max-w-lg">
-            <div className="flex justify-center mb-4">
-              <div className="bg-k1 p-2 rounded-md">
+        <div className="flex items-center justify-center mt-8 2xl:mt-20">
+          <div className="bg-white rounded-lg border p-6 sm:p-8 lg:p-10 w-full max-w-lg">
+            <div className="flex justify-center mb-2 2xl:mb-4">
+              <div className="bg-k1 p-1 2xl:p-2 rounded-md">
                 <PersonRoundedIcon className="text-white" fontSize="large" />
               </div>
             </div>
             <div className="flex flex-col items-center">
-              <h2 className="text-xl font-PSB">Get Started</h2>
+              <h2 className="text-lg 2xl:text-xl font-PSB">Get Started</h2>
               <p className="text-gray-400">Don't have an account?</p>
             </div>
-            <div className="flex justify-center mt-5 mb-8">
+            <div className="flex justify-center mt-5 mb-5">
               <div className="h-px w-full bg-gray-200"></div>
             </div>
             <form >
@@ -94,7 +94,7 @@ function Login() {
                 <div>
                   <label
                     htmlFor="userID"
-                    className="block text-lg font-PSB text-gray-700 mb-2"
+                    className="block text-md 2xl:text-lg font-PSB text-gray-700 mb-2"
                   >
                     India Post Office ID
                   </label>
@@ -109,14 +109,14 @@ function Login() {
                       value={formData.userid}
                       onChange={handleChange}
                       
-                      className="border rounded-md pl-10 py-3 w-full bg-[#F7F7F9] focus:outline-none focus:ring-k1 focus:border-k1 text-gray-600"
+                      className="border rounded-md pl-10 py-2 2xl:py-3 w-full bg-[#F7F7F9] focus:outline-none focus:ring-k1 focus:border-k1 text-gray-600"
                     />
                   </div>
                 </div>
                 <div>
                   <label
                     htmlFor="password"
-                    className="block text-lg font-PSB text-gray-700 mb-2"
+                    className="block text-md 2xl:text-lg font-PSB text-gray-700 mb-2"
                   >
                     Password
                   </label>
@@ -130,7 +130,7 @@ function Login() {
                       placeholder="Password"
                       value={formData.password}
                       onChange={handleChange}
-                      className="border rounded-md pl-10 py-3 w-full bg-[#F7F7F9] focus:outline-none focus:ring-k1 focus:border-k1 text-gray-600"
+                      className="border rounded-md pl-10 py-2 2xl:py-3 w-full bg-[#F7F7F9] focus:outline-none focus:ring-k1 focus:border-k1 text-gray-600"
                     />
                   </div>
                 </div>
@@ -138,27 +138,27 @@ function Login() {
               <div className="text-right mt-4">
                 <a
                   href="#"
-                  className="relative font-medium font-PSB text-k1 group"
+                  className="relative font-PM text-sm font-PSB text-k1 group"
                 >
                   Forgot password?
                   <span className="absolute left-1/2 bottom-[-8px] transform -translate-x-1/2 w-0 h-0.5 bg-k1 group-hover:w-1/2 transition-all duration-300 ease-in-out"></span>
                 </a>
               </div>
-              <div className="flex justify-center mt-6">
+              <div className="flex justify-center mt-5">
                 <div className="h-px w-full bg-gray-200"></div>
               </div>
               <div className="flex justify-end flex-col  mt-2 align-middle">
                 <button
                   type="button"
                   onClick={() => navigate("/register")}
-                  className="border border-k1 bg-white text-black font-bold py-3 px-5 rounded-md"
+                  className="border border-k1 bg-white text-black font-bold py-2 2xl:py-3 rounded-md"
                 >
                   <CreateRoundedIcon className="ml-[-2px] mr-2" />
                   Register
                 </button>
                 <button
                   type="button"
-                  className="bg-k1 text-white font-bold py-3 w-full mt-2 px-5 rounded-md"
+                  className="bg-k1 text-white font-bold py-2 2xl:py-3 w-full mt-2 rounded-md"
                   onClick={navigate("/services")}
                 >
                   <LoginRoundedIcon className="ml-[-2px] mr-2" />
